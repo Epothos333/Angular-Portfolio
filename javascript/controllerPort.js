5ab11ec9284722286287fb9c6cf2a60365f27be4
Edwin A <ea976@nyu.edu>
2016-03-26 20:57:34 -0400

    Holy god the styling. Things have centered on portfolio page

--- javascript/controllerPort.js
+++ javascript/controllerPort.js
@@ -1,23 +1,33 @@
 app.controller('portController', [ '$scope', function($scope) {
 
+	var divs = document.getElementsByClassName('ani');
+	$scope.reversearoo = function() {
+		for (i = 0; i <3; i++) {
+			if (divs[i].id === 'left' || divs[i].id ==='lefty') {
+				divs[i].id = 'righty';
+			} 
+			else if (divs[i].id === 'middle' || divs[i].id ==='middley') {
+				divs[i].id = 'lefty';
+			}
+			else {
+				divs[i].id = 'middley'
+			}
+		}
 
-			var buttons = document.getElementsByTagName('button');
-			var divs = document.getElementsByClassName('ani')
-			console.log(divs)
+	}
 
-
-			$scope.switcharoo = function() {
-				for (var i = 0; i <3; i++) {
-					if (divs[i].id === 'left') {
-						divs[i].id = 'middle';
-					} 
-					else if (divs[i].id === 'middle') {
-						divs[i].id = 'right';
-					}
-					else {
-						divs[i].id = 'left'
-					}
-				}
-		};
+	$scope.switcharoo = function() {
+		for (i = 0; i <3; i++) {
+			if (divs[i].id === 'left' || divs[i].id ==='lefty') {
+				divs[i].id = 'middle';
+			} 
+			else if (divs[i].id === 'middle' || divs[i].id ==='middley') {
+				divs[i].id = 'right';
+			}
+			else {
+				divs[i].id = 'left'
+			}
+		}
+};
 
 }]);
